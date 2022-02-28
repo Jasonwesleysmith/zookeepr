08aa1469a6ca8dcbeeee591d709aa6bc652f06bb
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-02-27 23:42:07 -0600

    refactor

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const animalRoutes = require('./animalRoutes');
+const zookeeperRoutes = require('./zookeeperRoutes');
+
+router.use(animalRoutes);
+router.use(zookeeperRoutes);
+
+module.exports = router;
+
+// router.use(require('./zookeeperRoutes'));
